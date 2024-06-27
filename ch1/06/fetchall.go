16bf609991e41e2a81c418a62c96f5d48feb9c58
Yhonatan Jesus <yiquiapaza@gmail.com>
2024-06-27 14:01:06 -0500

    feat: chapter 01

--- ch1/06/fetchall.go
+++ ch1/06/fetchall.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
@@ -28,7 +27,7 @@ func fetch(url string, ch chan<- string) {
 		ch <- fmt.Sprint(err)
 		return
 	}
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	nbytes, err := io.Copy(io.Discard, resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		ch <- fmt.Sprintf("While reading %s: %v", url, err)
